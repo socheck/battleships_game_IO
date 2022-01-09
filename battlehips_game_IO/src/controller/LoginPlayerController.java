fa998465f43a1920375cc68d6766e073555578f4
krzysztofd1235 <krzysztofduda99@wp.pl>
2022-01-09 21:35:32 +0100

    ss

--- battlehips_game_IO/src/controller/LoginPlayerController.java
+++ battlehips_game_IO/src/controller/LoginPlayerController.java
@@ -1,11 +1,13 @@
 package controller;
 
+import db.DbConnection;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 
 public class LoginPlayerController {
     @FXML
@@ -18,6 +20,13 @@ public class LoginPlayerController {
     public PasswordField passwordTextField;
     @FXML
     public Label playerLoginLabel;
+    private DbConnection dbConnection;
+
+
+    public void initialize(){
+        dbConnection = new DbConnection();
+        
+    }
 
     @FXML
     public void exitAction(){
@@ -26,6 +35,8 @@ public class LoginPlayerController {
     @FXML
     public void loginAction(){
         ((Stage) loginButton.getScene().getWindow()).close();
+         String login = loginTextField.getText();
+         String password = passwordTextField.getText();
 
     }
 
